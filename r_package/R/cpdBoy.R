087960e3a5c21da5577aac54c9f43c7459383260
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-11-18 15:35:37 +0100

    Added more stuff

--- r_package/R/cpdBoy.R
+++ r_package/R/cpdBoy.R
@@ -1,25 +1,29 @@
 # Load the 'httr' library
-library(httr)
-library(jsonlite)
-
-# Define the API endpoint
-url <- "127.0.0.1:3000/message"
-
-# Create a list representing the JSON body
-json_body <- list(
-  text = "cpd00058"
-)
-
-# Convert the list to JSON
-json_body <- toJSON(json_body, auto_unbox = TRUE)
-
-# Send the POST request
-response <- POST(
-  url, 
-  body = json_body, 
-  encode = "json", 
-  add_headers("Content-Type" = "application/json")
-)
-
-# Print the response
-print(content(response, "text"))
+#' Does things
+#' @export
+d <- function() {
+  library(httr)
+  library(jsonlite)
+  
+  # Define the API endpoint
+  url <- "127.0.0.1:3000/message"
+  
+  # Create a list representing the JSON body
+  json_body <- list(
+    text = "cpd00058"
+  )
+  
+  # Convert the list to JSON
+  json_body <- toJSON(json_body, auto_unbox = TRUE)
+  
+  # Send the POST request
+  response <- POST(
+    url, 
+    body = json_body, 
+    encode = "json", 
+    add_headers("Content-Type" = "application/json")
+  )
+  
+  # Print the response
+  print(content(response, "text"))
+}
