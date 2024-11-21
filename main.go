4b3b34b1a31e1bb1262f0c7f7b04e2f7ce9cd435
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-11-21 14:17:47 +0100

    Removed usesless stuff

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
   //Init db and slog
   myHandler := &server.DataBaseHandler {}
   logger := slog.New(slog.NewJSONHandler(logFile, nil))
-  slog.LogAttrs(context.Background(), slog.LevelInfo, "Starting the server")
+  logger.LogAttrs(context.Background(), slog.LevelInfo, "Starting the server")
   slog.SetDefault(logger)
   db, err := sql.Open("sqlite3", "./my_database.db")
   myHandler.Db = *db
